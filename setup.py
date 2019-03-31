cca6bb6808fcef8b00be4354e75465d19d48cac0
shubham bhatia <mitrandamac@shubhams-MBP.fios-router.home>
2019-03-31 15:36:39 -0400

    Setup.py to enable building

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='snapshotalyzer-30000',
+    version='0.1',
+    author='Shubham Bhatia',
+    description='shbhatia@syr.edu',
+    liscence='GPLv3+',
+    packages=['shotty'],
+    url='https://github.com/shubhamb0002/snapshotanalyzer-30000',
+    install_requires=[
+        'click',
+        'boto3'
+        ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+        ''',
+        
+
+)
