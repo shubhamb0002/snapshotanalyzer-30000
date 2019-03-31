42f10702009630ec3ddb23c5eca0a59dbeef52bf
shubham bhatia <mitrandamac@shubhams-MBP.fios-router.home>
2019-03-31 14:13:28 -0400

    Wait for instances to stop and start while taking a Snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -79,9 +79,19 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances.all():
+        print("Stoping...{0}".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
         for v in i.volumes.all():
             print("Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="created by an snapshotAlyzer 30000")
+
+        print("Starting...{0}".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job is done!!")
+
     return
 
 @instances.command('list')
